602e5d61d4b506b311619d5d98789b93220f01c9
00wz <130168402+00wz@users.noreply.github.com>
2023-07-15 22:53:52 +0300

    2.0

--- Assets/Scripts/NetworkTransformTest.cs
+++ Assets/Scripts/NetworkTransformTest.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.Netcode;
 using UnityEngine;
 
-public class NetworkTransformTest : MonoBehaviour
+public class NetworkTransformTest : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (IsServer)
+        {
+            float theta = Time.frameCount / 10.0f;
+            transform.position = new Vector3((float)Math.Cos(theta), 0.0f, (float)Math.Sin(theta));
+        }
     }
 }
